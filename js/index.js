c95cbbfdcdc5df2df4ff480faf555d348050f714
Oliver Garcia <ngarcia333@gmail.com>
2022-01-16 13:19:24 +0100

    Iteration 1-3

--- js/index.js
+++ js/index.js
@@ -6,4 +6,17 @@ console.log (`The navigator's name is ${hacker2}`);
 
 console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
 
-console.log (hacker1.toUpperCase());
+console.log (hacker1.split("").join(" "));
+
+console.log (hacker2.split("").reverse().join(""));
+
+let result= hacker1.localeCompare(hacker2);
+
+if (result > 0){
+    console.log("The driver's name goes first.")
+} else if( result < 0){
+    console.log("Yo, the navigator goes first definitely.")
+} else {
+    console.log("What?! You both have the same name?")
+}
+
